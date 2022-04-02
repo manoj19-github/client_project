0f82087d1c8ac3f3d9d02dd927d3161792a42c1d
Sudipta <das.sudipta94@gmail.com>
2022-04-02 13:37:32 +0530

    fixed

--- src/Views/Zone/EditZone/EditZoneView.tsx
+++ src/Views/Zone/EditZone/EditZoneView.tsx
@@ -39,85 +39,87 @@ function ZoneEditView({ submitData, SingleZone }: ZoneEditViewProps) {
         />
         <CardContent>
           <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container justifyContent="center" alignItems={"center"}>
+              <Grid item xs={5}>
+                <Grid
+                  container
+                  paddingTop={2}
+                  justifyContent="left"
+                  alignItems={"center"}
+                >
+                  <Grid item xs={11.5}>
+                    <TextField
+                      label="Zone Code"
+                      {...register("zone_code", { required: true })}
+                      style={{ width: "100%", margin: "5px" }}
+                      type="text"
+                      variant="outlined"
+                      size="small"
+                      name="zone_code"
+                      placeholder="Zone Code"
+                      error={!!errors["zone_code"]}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={5}>
+                <Grid
+                  container
+                  flexDirection={"row"}
+                  paddingTop={2}
+                  justifyContent="right"
+                  alignItems={"center"}
+                >
+                  <Grid item xs={11.5}>
+                    <TextField
+                      {...register("zone_name", { required: true })}
+                      style={{ width: "100%", margin: "5px" }}
+                      type="text"
+                      variant="outlined"
+                      size="small"
+                      label="Zone Name"
+                      name="zone_name"
+                      placeholder="Zone Name"
+                      error={!!errors["zone_name"]}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
             <Grid
               container
-              paddingTop={2}
               justifyContent="center"
               alignItems={"center"}
+              paddingTop={2}
             >
-              <Grid item xs={4}>
-                <Typography>Zone Code</Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <Typography>:</Typography>
-              </Grid>
-              <Grid item xs={5}>
-                <TextField
-                  {...register("zone_code", { required: true })}
-                  style={{ width: "100%", margin: "5px" }}
-                  type="text"
-                  variant="outlined"
-                  size="small"
-                  name="zone_code"
-                  placeholder="Type Zone Code"
-                  error={!!errors["zone_code"]}
-                />
-              </Grid>
-            </Grid>
-            <Grid container justifyContent="center" alignItems={"center"}>
-              <Grid item xs={4}>
-                <Typography>Zone Name</Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <Typography>:</Typography>
-              </Grid>
-              <Grid item xs={5}>
-                <TextField
-                  {...register("zone_name", { required: true })}
-                  style={{ width: "100%", margin: "5px" }}
-                  type="text"
-                  variant="outlined"
-                  size="small"
-                  name="zone_name"
-                  placeholder="Type Zone Name"
-                  error={!!errors["zone_name"]}
-                />
-              </Grid>
-            </Grid>
-            <Grid container justifyContent="center" alignItems={"center"}>
-              <Grid item xs={4}>
-                <Typography>Description</Typography>
-              </Grid>
-              <Grid item xs={1}>
-                <Typography>:</Typography>
-              </Grid>
-              <Grid item xs={5}>
+              <Grid item xs={10}>
                 <TextField
                   {...register("zone_desc", { required: true })}
                   style={{ width: "100%", margin: "5px" }}
                   multiline
                   rows={4}
+                  label="Description"
                   variant="outlined"
                   name={"zone_desc"}
-                  placeholder="Type Description"
+                  placeholder="Description"
                   error={!!errors["zone_desc"]}
                 />
               </Grid>
             </Grid>
             <Grid container justifyContent="center" alignItems={"center"}>
-              <Button
-                onClick={() => history.push(`/zone`)}
-                style={{
-                  color: "#fff",
-                  marginRight: 10,
-                  background: "#C1B4B3",
-                }}
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Back
-              </Button>
               <Grid item>
+                <Button
+                  onClick={() => history.push(`/zone`)}
+                  style={{
+                    color: "#fff",
+                    marginRight: 10,
+                    background: "#C1B4B3",
+                  }}
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Back
+                </Button>
                 <Button
                   type="submit"
                   style={{ color: "#fff" }}
